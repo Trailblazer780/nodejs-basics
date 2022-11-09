fba5be21cce941d9a1520ff8b4844b3369aedfea
Ethan Farrell <78605366+Trailblazer780@users.noreply.github.com>
2022-11-09 11:29:53 -0400

    ejs template engine, removed other template engine

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 // setting up app
 const app = express();
 
-app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 // routes
