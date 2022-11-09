27ee2c60d3412760f0be723ca238ebd5b1835f6c
Ethan Farrell <78605366+Trailblazer780@users.noreply.github.com>
2022-11-09 09:15:17 -0400

    pug template engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const rootDir  = require('../util/path');
 const express = require('express');
+const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', { prods: products, pageTitle: 'Shop', path:'/'});
 });
 
 module.exports = router;
