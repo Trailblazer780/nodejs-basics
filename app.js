bb7bcb3252294f0b5dcfad378186ba52d67af9d0
Ethan Farrell <78605366+Trailblazer780@users.noreply.github.com>
2022-11-15 11:16:51 -0400

    authentication added

--- app.js
+++ app.js
@@ -1,62 +1,75 @@
-const path = require('path');
-const mongoose = require('mongoose');
-const express = require('express');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const bodyParser = require('body-parser');
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const path = require("path");
+const mongoose = require("mongoose");
+const express = require("express");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const bodyParser = require("body-parser");
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
-const MONGODB_URI = 'mongodb+srv://trailblazer:trailblazer12345@cluster0.hrngep2.mongodb.net/shop?retryWrites=true&w=majority';
+const MONGODB_URI =
+  "mongodb+srv://trailblazer:trailblazer12345@cluster0.hrngep2.mongodb.net/shop?retryWrites=true&w=majority";
 
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'sessions'
+  collection: "sessions",
 });
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const csrfProtection = csrf();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
-  .then(user => {
-    req.user = user;
-    next();
-  })
-  .catch(err => console.log(err));
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
-
-mongoose.connect(MONGODB_URI).then(result => {
-  User.findOne().then(user => {
-    if (!user) {
-      const user = new User({
-        name: 'Ethan',
-        email: 'ethan@test.com',
-        cart: {
-          items: []
-        }
-      });
-      user.save();
-    }
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-  app.listen(3000);
-}).catch(err => { console.log(err) });
